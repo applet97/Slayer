e67fdd2eaf62a08dee2c1861e8ee11d105fd3439
Aiba <aiba.prenov@gmail.com>
2016-10-24 21:32:37 +0600

    tr

--- Slayer/main/management/commands/shuffle.py
+++ Slayer/main/management/commands/shuffle.py
@@ -16,16 +16,19 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         game = Game.objects.first()
-        entries = list(game.game_entries.filter(player__is_superuser=False, player__is_active=True))
-        random.shuffle(entries)
-        try:
-            for i in xrange(len(entries)-1):
-            	entries[i].victim = entries[i+1].player
-            	entries[i].save()
-            entries[len(entries)-1].victim = entries[0].player
-            entries[len(entries)-1].save()
-            game.shuffled = True
-            game.save()
-            print "Success"
-        except Exception as e:
-            print "Error %s" % str(e)
+        if game.shuffled:
+            print "Game is shuffled"
+        else:
+            entries = list(game.game_entries.filter(player__is_superuser=False, player__is_active=True))
+            random.shuffle(entries)
+            try:
+                for i in xrange(len(entries)-1):
+                    entries[i].victim = entries[i+1].player
+                    entries[i].save()
+                entries[len(entries)-1].victim = entries[0].player
+                entries[len(entries)-1].save()
+                game.shuffled = True
+                game.save()
+                print "Success"
+            except Exception as e:
+                print "Error %s" % str(e)
