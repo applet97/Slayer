c9ba96c100443c91b4d833f25a755bcf91e502aa
almat <almat_kenen@mail.ru>
2017-10-24 22:52:17 +0600

    bugs fixed

--- Slayer/run.sh
+++ Slayer/run.sh
@@ -1,3 +1,3 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn --bind 0.0.0.0:8001 --workers 3 --timeout 120 --worker-class gevent --log-level=INFO --access-logfile - --error-logfile - --user 0 --group 0 mlm.wsgi 
+gunicorn --bind 0.0.0.0:8001 --workers 3 --timeout 120 --worker-class gevent --log-level=INFO --access-logfile - --error-logfile - --user 0 --group 0 slayer.wsgi 
