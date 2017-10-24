463617d0061a620c41a6d8969b28b655570eff12
almat <almat_kenen@mail.ru>
2017-10-24 18:57:55 +0600

    bugs fixed

--- Slayer/server/fabfile/__init__.py
+++ Slayer/server/fabfile/__init__.py
@@ -7,6 +7,6 @@ import install
 
 env.repository = "https://github.com/applet97/Slayer.git"
 env.user = "ubuntu"
-env.hosts = ["18.194.186.183"]
+env.hosts = ["18.195.8.85"]
 env.key_filename = "~/slayer.pem"
 env.filename = "docker-compose.yml"
