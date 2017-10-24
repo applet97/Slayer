2af8317b37b11176d04f3cdc2cc32c66f769964f
almat <almat_kenen@mail.ru>
2017-10-24 18:46:35 +0600

    server conf added (1)

--- Slayer/server/fabfile/__init__.py
+++ Slayer/server/fabfile/__init__.py
@@ -7,6 +7,6 @@ import install
 
 env.repository = "https://github.com/applet97/Slayer.git"
 env.user = "ubuntu"
-env.hosts = ["52.57.136.204"]
+env.hosts = ["18.194.186.183"]
 env.key_filename = "~/slayer.pem"
 env.filename = "docker-compose.yml"
